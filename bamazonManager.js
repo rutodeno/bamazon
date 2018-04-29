de231ff63a82e515cf4c48d0e467e86556ed7842
Dennis Ruto <rutodeno@gmail.com>
2018-04-28 23:35:36 -0400

    updated file

--- bamazonManager.js
+++ bamazonManager.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: "3306",
     user: "root",
-    password: "Kipsang1990",
+    password: "***********",
     database: "bamazon"
 });
 
